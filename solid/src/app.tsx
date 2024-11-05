2ea9549d5121283323dc86fb6dfccefda41f20df
Philipp Spiess <hello@philippspiess.com>
2024-11-05 18:10:51 +0100

    Add Solid

---
+++ solid/src/app.tsx
@@ -0,0 +1,22 @@
+import { MetaProvider, Title } from "@solidjs/meta";
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import "./app.css";
+
+export default function App() {
+  return (
+    <Router
+      root={(props) => (
+        <MetaProvider>
+          <Title>SolidStart - Basic</Title>
+          <Suspense>
+            <div class="p-2">{props.children}</div>
+          </Suspense>
+        </MetaProvider>
+      )}
+    >
+      <FileRoutes />
+    </Router>
+  );
+}
---
+++ solid/src/routes/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 class="font-bold">Welcome to SolidStart</h1>
+      <p>
+        Visit{" "}
+        <a href="https://start.solidjs.com" target="_blank">
+          start.solidjs.com
+        </a>{" "}
+        to learn how to build SolidStart apps.
+      </p>
+    </main>
+  );
+}
