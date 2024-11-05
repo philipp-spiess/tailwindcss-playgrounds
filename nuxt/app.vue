c458b15ba8001828da021ae0b5982ff769c1f494
Philipp Spiess <hello@philippspiess.com>
2024-11-05 17:33:16 +0100

    Add Nuxt

---
+++ nuxt/app.vue
@@ -0,0 +1,13 @@
+<script>
+import './src/tailwind.css'
+</script>
+
+<template>
+  <div class="p-2">
+    <NuxtRouteAnnouncer />
+    <h1 class="font-bold">Welcome to Nuxt</h1>
+    <p>
+      Visit <a href="https://nuxt.com">nuxt.com</a> to read the documentation
+    </p>
+  </div>
+</template>
