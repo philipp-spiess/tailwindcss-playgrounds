11fe58cffe77ad85451d051883a86f6fd6a0fc9e
Philipp Spiess <hello@philippspiess.com>
2024-11-20 12:16:44 +0100

    Add Nextjs example

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-bold">Welcome to Next.js</h1>
+      <p>
+        Visit <a href="https://nextjs.org/learn">nextjs.org/learn</a> to read
+        the documentation
+      </p>
+    </>
+  );
+}
