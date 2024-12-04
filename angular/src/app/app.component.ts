02ba4850787e11a696109840cea8b50b2b095ff4
Philipp Spiess <hello@philippspiess.com>
2024-12-04 10:28:10 +0100

    Add Angular example

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {}
